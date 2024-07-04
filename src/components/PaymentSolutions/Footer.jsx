43a3846d3efa40533c3a7c805a95cdf3fa69e883
abdallahmoustafa94 <dimo.am@gmail.com>
2024-07-04 23:32:02 +0300

    added footer

--- src/components/PaymentSolutions/Footer.jsx
+++ src/components/PaymentSolutions/Footer.jsx
@@ -5,65 +5,33 @@ const Footer = () => {
     <footer className="style-14">
 
       <div className="container">
-        {/* <div className="content section-padding">
+        <div className="content section-padding">
           <div className="row justify-content-between gx-0">
             <div className="col-lg-4">
               <div className="foot-info">
                 <div className="foot-logo">
-                  <img src="/assets/img/logo_14.png" alt="" />
+                  <img src="/assets/img/logo.jpg" alt="" />
                 </div>
-                <p> Purchases subject to credit approval, and the iTeck Cashback Mastercard must be used for payment. </p>
                 <div className="social-icons">
-                  <a href="#"> <i className="fab fa-facebook-f"></i> </a>
-                  <a href="#"> <i className="fab fa-twitter"></i> </a>
-                  <a href="#"> <i className="fab fa-behance"></i> </a>
-                  <a href="#"> <i className="fab fa-youtube"></i> </a>
+                  <a href="https://www.facebook.com/MADA.Plastic.Solutions?mibextid=LQQJ4d"> <i className="fab fa-facebook-f" style={{fontSize:"30px"}}></i> </a>
+                  <a href="https://www.linkedin.com/company/mada-pack/"> <i className="fab fa-linkedin" style={{fontSize:"30px"}}></i> </a>
+                  <a href="#"> <i className="fab fa-instagram" style={{fontSize:"30px"}}></i> </a>
+
                 </div>
               </div>
             </div>
-            <div className="col-lg-2">
-              <div className="links">
-                <h6 className="sub-link text-uppercase mb-20 mt-5 mt-lg-0"> Essential Pages </h6>
+            <div className="col-lg-8">
+                <h4 className="sub-link text-uppercase mb-20 mt-5 mt-lg-0"> تواصل معنا </h4>
                 <ul>
-                  <li> <a href="#"> Help </a> </li>
-                  <li> <a href="#"> Shop </a> </li>
-                  <li> <a href="#"> Contact </a> </li>
-                  <li> <a href="#"> Enterprise </a> </li>
-                  <li> <a href="#"> Fees </a> </li>
-                  <li> <a href="#"> Partners </a> </li>
-                  <li> <a href="#"> Security </a> </li>
-                  <li> <a href="#"> Feedback </a> </li>
-                  <li> <a href="#"> Apps </a> </li>
+                  <li>  العنوان : المنطقة الصناعية الجديدة امتداد الرابعة قطعة ٤ بلوك ٩ مدينة برج العرب الجديدة الاسكندرية </li>
+                  <li className="mb-2 mt-2"> <a href="tel:01114447801" > تليفون : 01114447801</a> </li>
+                  <li className="mb-2"> <a href="tel:تليفون : 111265773" > تليفون : 01112657733</a> </li>
+                  <li className="mt-2"> <a href="mailto:info@mada-pack.com"> البريد الالكتروني  :  info@mada-pack.com </a> </li>
                 </ul>
-              </div>
             </div>
-            <div className="col-lg-2">
-              <div className="links">
-                <h6 className="sub-link text-uppercase mb-20 mt-5 mt-lg-0"> About company </h6>
-                <ul>
-                  <li> <a href="#"> About </a> </li>
-                  <li> <a href="#"> Values </a> </li>
-                  <li> <a href="#"> Newsroom </a> </li>
-                  <li> <a href="#"> In Action </a> </li>
-                  <li> <a href="#"> Jobs </a> </li>
-                  <li> <a href="#"> Public Policy </a> </li>
-                  <li> <a href="#"> Investor </a> </li>
-                  <li> <a href="#"> FAQ </a> </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-2">
-              <div className="links">
-                <h6 className="sub-link text-uppercase mb-20 mt-5 mt-lg-0"> download app </h6>
-                <div className="text mb-20"> Available in any kind of ready version. </div>
-                <div className="down-imgs">
-                  <a href="#" className="mb-15"> <img src="/assets/img/header/head_14_btn_apple.png" alt="" /> </a>
-                  <a href="#"> <img src="/assets/img/header/head_14_btn_android.png" alt="" /> </a>
-                </div>
-              </div>
-            </div>
+     
           </div>
-        </div> */}
+        </div>
         <div className="foot border-1 border-top brd-gray py-3">
           <div className="row align-items-center">
        
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,15 +5,10 @@ import MainLayout from '@layouts/Main';
 //= Components
 import Navbar from '@components/Navbars/PaymentSolutionsNav';
 import Header from '@components/PaymentSolutions/Header';
-import Clients from '@components/PaymentSolutions/Clients';
-import Features from '@components/PaymentSolutions/Features';
+
 import ChooseUs from '@components/PaymentSolutions/ChooseUs';
 import Screenshots from '@components/PaymentSolutions/Screenshots';
-import Numbers from '@components/PaymentSolutions/Numbers';
-import About from '@components/PaymentSolutions/About';
-import Contact from '@components/PaymentSolutions/Contact';
-import Testimonials from '@components/PaymentSolutions/Testimonials';
-import Blog from '@components/PaymentSolutions/Blog';
+
 import Footer from '@components/PaymentSolutions/Footer';
 
 const HomePaymentSolutions = () => {
